3a41cb140163ce241d77e974df862c8143c36429
Tom Rees <zephod@gmail.com>
2013-02-08 13:19:25 +0000

    [162] Cleaning up IE7 issues.

--- ckanext/ga_report/public/scripts/ckanext_ga_reports.js
+++ ckanext/ga_report/public/scripts/ckanext_ga_reports.js
@@ -103,8 +103,8 @@ CKAN.GA_Reports.bind_month_selector = function() {
   var handler = function(e) { 
     var target = $(e.delegateTarget);
     var form = target.closest('form');
-    form.attr('action', form.attr('action')+window.location.hash);
-    form.submit();
+    var url = form.attr('action')+'?month='+target.val()+window.location.hash;
+    window.location = url;
   };
   var selectors = $('select[name="month"]');
   assert(selectors.length>0);
